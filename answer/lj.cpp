15192926b652b559d57e43bab23e9e190d32a652
H. Watanabe <kaityo@users.sourceforge.jp>
2020-06-25 00:08:41 +0900

    adds check code and answer

--- answer/lj.cpp
+++ answer/lj.cpp
@@ -91,7 +91,91 @@ void calc_force_simple(void) {
 
 // SIMD化した関数
 void calc_force_simd(void) {
-  for (int i = 0; i < N - 1; i++) {
+  const __m256d vc24 = _mm256_set_pd(24 * dt, 24 * dt, 24 * dt, 24 * dt);
+  const __m256d vc48 = _mm256_set_pd(48 * dt, 48 * dt, 48 * dt, 48 * dt);
+
+  for (int i = 0; i < N-1; i++) {
+    __m256d vqi = _mm256_load_pd((double *)(q + i));
+    double qix = vqi[X];
+    double qiy = vqi[Y];
+    double qiz = vqi[Z];
+
+    __m256d vpi = _mm256_load_pd((double *)(p + i));
+    int j = i + 1;
+    for (; j + 3 < N; j += 4) {
+      int j_0 = j;
+      int j_1 = j + 1;
+      int j_2 = j + 2;
+      int j_3 = j + 3;
+
+      __m256d vqj_0 = _mm256_load_pd((double *)(q + j_0));
+      __m256d vqj_1 = _mm256_load_pd((double *)(q + j_1));
+      __m256d vqj_2 = _mm256_load_pd((double *)(q + j_2));
+      __m256d vqj_3 = _mm256_load_pd((double *)(q + j_3));
+
+      __m256d vdr_0 = vqj_0 - vqi;
+      __m256d vdr_1 = vqj_1 - vqi;
+      __m256d vdr_2 = vqj_2 - vqi;
+      __m256d vdr_3 = vqj_3 - vqi;
+
+      __m256d tmp0 = _mm256_unpacklo_pd(vdr_0, vdr_1);
+      __m256d tmp1 = _mm256_unpackhi_pd(vdr_0, vdr_1);
+      __m256d tmp2 = _mm256_unpacklo_pd(vdr_2, vdr_3);
+      __m256d tmp3 = _mm256_unpackhi_pd(vdr_2, vdr_3);
+
+      __m256d vdx = _mm256_permute2f128_pd(tmp0, tmp2, 0x20);
+      __m256d vdy = _mm256_permute2f128_pd(tmp1, tmp3, 0x20);
+      __m256d vdz = _mm256_permute2f128_pd(tmp0, tmp2, 0x31);
+
+      __m256d vr2 = vdx * vdx + vdy * vdy + vdz * vdz;
+      __m256d vr6 = vr2 * vr2 * vr2;
+      __m256d vdf = (vc24 * vr6 - vc48) / (vr6 * vr6 * vr2);
+
+      __m256d vdf_0 = _mm256_permute4x64_pd(vdf, 0);
+      __m256d vdf_1 = _mm256_permute4x64_pd(vdf, 85);
+      __m256d vdf_2 = _mm256_permute4x64_pd(vdf, 170);
+      __m256d vdf_3 = _mm256_permute4x64_pd(vdf, 255);
+ 
+      __m256d vpj_0 = _mm256_load_pd((double *)(p + j_0));
+      __m256d vpj_1 = _mm256_load_pd((double *)(p + j_1));
+      __m256d vpj_2 = _mm256_load_pd((double *)(p + j_2));
+      __m256d vpj_3 = _mm256_load_pd((double *)(p + j_3));
+      vpj_0 -= vdf_0 * vdr_0;
+      vpj_1 -= vdf_1 * vdr_1;
+      vpj_2 -= vdf_2 * vdr_2;
+      vpj_3 -= vdf_3 * vdr_3;
+      _mm256_store_pd(&(p[j_0][0]), vpj_0);
+      _mm256_store_pd(&(p[j_1][0]), vpj_1);
+      _mm256_store_pd(&(p[j_2][0]), vpj_2);
+      _mm256_store_pd(&(p[j_3][0]), vpj_3);
+      vpi += vdf_0 * vdr_0;
+      vpi += vdf_1 * vdr_1;
+      vpi += vdf_2 * vdr_2;
+      vpi += vdf_3 * vdr_3;
+    }
+    _mm256_store_pd(&(p[i][0]), vpi);
+
+    for (; j < N; j++) {
+      double dx = q[j][X] - qix;
+      double dy = q[j][Y] - qiy;
+      double dz = q[j][Z] - qiz;
+      double r2 = dx * dx + dy * dy + dz * dz;
+      double r6 = r2 * r2 * r2;
+      double df = (24.0 * r6 - 48.0) / (r6 * r6 * r2) * dt;
+      p[j][X] -= df * dx;
+      p[j][Y] -= df * dy;
+      p[j][Z] -= df * dz;
+      p[i][X] += df * dx;
+      p[i][Y] += df * dy;
+      p[i][Z] += df * dz;
+    }
+  }
+}
+
+
+// まず、ループを4倍展開した関数
+void calc_force_unrolled(void) {
+  for (int i = 0; i < N-1; i++) {
     // i粒子の座標と運動量を受け取っておく (内側のループでiは変化しないから)
 
     // 4成分をまとめてロードする
@@ -120,8 +204,67 @@ void calc_force_simd(void) {
     double piy = p[i][Y];
     double piz = p[i][Z];
     */
+    int j = i + 1;
+    for (; j + 3 < N; j += 4) {
+      int j_0 = j;
+      int j_1 = j + 1;
+      int j_2 = j + 2;
+      int j_3 = j + 3;
 
-    for (int j = i + 1; j < N; j++) {
+      double dx_0 = q[j_0][X] - qix;
+      double dy_0 = q[j_0][Y] - qiy;
+      double dz_0 = q[j_0][Z] - qiz;
+      double r2_0 = dx_0 * dx_0 + dy_0 * dy_0 + dz_0 * dz_0;
+      double r6_0 = r2_0 * r2_0 * r2_0;
+      double df_0 = (24.0 * r6_0 - 48.0) / (r6_0 * r6_0 * r2_0) * dt;
+      p[j_0][X] -= df_0 * dx_0;
+      p[j_0][Y] -= df_0 * dy_0;
+      p[j_0][Z] -= df_0 * dz_0;
+      pix += df_0 * dx_0;
+      piy += df_0 * dy_0;
+      piz += df_0 * dz_0;
+
+      double dx_1 = q[j_1][X] - qix;
+      double dy_1 = q[j_1][Y] - qiy;
+      double dz_1 = q[j_1][Z] - qiz;
+      double r2_1 = dx_1 * dx_1 + dy_1 * dy_1 + dz_1 * dz_1;
+      double r6_1 = r2_1 * r2_1 * r2_1;
+      double df_1 = (24.0 * r6_1 - 48.0) / (r6_1 * r6_1 * r2_1) * dt;
+      p[j_1][X] -= df_1 * dx_1;
+      p[j_1][Y] -= df_1 * dy_1;
+      p[j_1][Z] -= df_1 * dz_1;
+      pix += df_1 * dx_1;
+      piy += df_1 * dy_1;
+      piz += df_1 * dz_1;
+
+      double dx_2 = q[j_2][X] - qix;
+      double dy_2 = q[j_2][Y] - qiy;
+      double dz_2 = q[j_2][Z] - qiz;
+      double r2_2 = dx_2 * dx_2 + dy_2 * dy_2 + dz_2 * dz_2;
+      double r6_2 = r2_2 * r2_2 * r2_2;
+      double df_2 = (24.0 * r6_2 - 48.0) / (r6_2 * r6_2 * r2_2) * dt;
+      p[j_2][X] -= df_2 * dx_2;
+      p[j_2][Y] -= df_2 * dy_2;
+      p[j_2][Z] -= df_2 * dz_2;
+      pix += df_2 * dx_2;
+      piy += df_2 * dy_2;
+      piz += df_2 * dz_2;
+
+      double dx_3 = q[j_3][X] - qix;
+      double dy_3 = q[j_3][Y] - qiy;
+      double dz_3 = q[j_3][Z] - qiz;
+      double r2_3 = dx_3 * dx_3 + dy_3 * dy_3 + dz_3 * dz_3;
+      double r6_3 = r2_3 * r2_3 * r2_3;
+      double df_3 = (24.0 * r6_3 - 48.0) / (r6_3 * r6_3 * r2_3) * dt;
+      p[j_3][X] -= df_3 * dx_3;
+      p[j_3][Y] -= df_3 * dy_3;
+      p[j_3][Z] -= df_3 * dz_3;
+      pix += df_3 * dx_3;
+      piy += df_3 * dy_3;
+      piz += df_3 * dz_3;
+    }
+
+    for (; j < N; j++) {
       double dx = q[j][X] - qix;
       double dy = q[j][Y] - qiy;
       double dz = q[j][Z] - qiz;
@@ -147,7 +290,6 @@ void calc_force_simd(void) {
     */
   }
 }
-
 
 // インテルコンパイラのループ交換最適化阻害のためのダミー変数
 int sum = 0;
